a08a0c2c3efe05972be6c968973c3fb6c357163b
manishsaraan <manish021js@gmail.com>
2017-10-28 22:22:34 +0530

    clean task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,32 @@
+var grunt = require('grunt');
 module.exports = function(grunt){
     grunt.initConfig({
     	pkg: grunt.file.readJSON('package.json'),
+        clean : {
+             output : ['demo/*']
+        },
     	uglify:{
-            my_target:{
-            	files: [{
-            		expand: true,
-            		cwd: 'js/',
-            		src: ['*.js','!*.min.js'],
-            		dest: 'js/'
-            		ext : '.js'
-            	}]
-            }
+    		my_target : {
+    			 files:{'js/main.min.js'  : 'js/*.js'} 
+    			}           
     	},
-    	cssmin:{
-    		my_target:{
-    			files: [{
-    				expand: true,
-    				cwd: 'css/',
-    				src : ['*.css','!*.min.css'],
-    				dest: 'css/',
-    				ext : '.min.css'
-    			}]
-    		}
-    	}
+        cssmin:{
+            my_target:{
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src : ['*.css','!*.min.css'],
+                    dest: 'css/',
+                    ext : '.min.css'
+                }]
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');    
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');    	
+   
+
 };
+  grunt.registerTask('default', ['clean', 'uglify','cssmin']);
